6e8b839f460f6d2aa970274b7b82c0ff074db08b
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-03-29 20:20:08 -0600

    ajustes

--- js/alto.js
+++ js/alto.js
@@ -1,8 +1,13 @@
 
-console.log("alto");
-
-
 $(document).ready(function(){
     $("#fondo").css({"height":$(window).height() + "px"});
 });
 
+$(window).scroll(function () {
+        if ($("#menu").offset().top > 56) {
+            $("#menu").addClass("fondo-nav");
+
+        } else {
+            $("#menu").removeClass("fondo-nav");
+        }
+    });
